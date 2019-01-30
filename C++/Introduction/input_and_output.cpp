d56270d56a20fc4ef0ae4f4c60bf0bc3b84f5caa
Junwu Zhang <junwuzhang@DNa1c44ab.SUNet>
2019-01-30 01:04:35 -0800

    test commit

---
+++ C++/Introduction/input_and_output.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int num1, num2, num3;
+
+    cin >> num1 >> num2 >> num3;
+    int sum = num1 + num2 + num3;
+    cout << sum << endl;
+
+    return 0;
+}
