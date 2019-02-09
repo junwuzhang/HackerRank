6ca61bee956cb08ba3bff59197e45a614138434f
Junwu Zhang <junwuzhang@DN0a62b3eb.SUNet>
2019-02-08 16:51:59 -0800

    reduced graph question time complexity

---
+++ Interview_Prep/9_Graphs/roads_and_libraries.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+vector<int> neighbors[100005];
+long clusterSize;
+bool visited[100005];
+
+// Use DFS to find out how many nodes are in one cluster
+void clusterSizeDFS(int currentNode)
+{
+    clusterSize++;
+    visited[currentNode] = true;
+    for (auto neighbor : neighbors[currentNode])
+    {
+        if (!visited[neighbor])
+        {
+            clusterSizeDFS(neighbor);
+        }
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++)
+    {   
+        int n, m, fromCity, toCity;
+        long c_lib, c_road;
+        cin >> n >> m >> c_lib >> c_road;
+
+        for (int i = 0; i <= n; i++)
+        {
+            neighbors[i].clear();
+            visited[i] = false;
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            cin >> fromCity >> toCity;
+            neighbors[fromCity].push_back(toCity);
+            neighbors[toCity].push_back(fromCity);
+        }
+
+        long totalCost = 0;
+
+        for (int currentNode = 1; currentNode <= n; currentNode++)
+        {
+            if (!visited[currentNode])
+            {
+                clusterSize = 0;
+                clusterSizeDFS(currentNode);
+                totalCost += c_lib;
+                if (c_lib < c_road)
+                {
+                    totalCost += (c_lib * (clusterSize - 1));
+                }
+                else
+                {
+                    totalCost += (c_road * (clusterSize - 1));
+                }
+            }
+        }
+
+        fout << totalCost << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
